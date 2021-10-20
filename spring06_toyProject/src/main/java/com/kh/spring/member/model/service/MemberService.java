71479b02ec9cb9242b08b96d7010fe864ae708e8
kijung <dlrlwjd1313@naver.com>
2021-10-20 17:03:51 +0900

    [spring study servlet -> spring code변환] 21/10/20

--- spring06_toyProject/src/main/java/com/kh/spring/member/model/service/MemberService.java
+++ spring06_toyProject/src/main/java/com/kh/spring/member/model/service/MemberService.java
@@ -5,17 +5,26 @@ import org.springframework.stereotype.Service;
 
 import com.kh.spring.member.model.dto.Member;
 import com.kh.spring.member.model.repository.MemberRepository;
+import com.kh.spring.member.validator.JoinForm;
 
 @Service
 public class MemberService {
 
-   @Autowired
-   private MemberRepository memberRepository;
+	@Autowired
+	private MemberRepository memberRepository;
 
-   public void insertMember(Member member) {
+	public void insertMember(JoinForm form) {
 
-      memberRepository.insertMember(member);
+		memberRepository.insertMember(form);
 
-   }
+	}
+
+	public Member authenticateUser(Member member) {
+			return memberRepository.authenticateUser(member);
+	}
+
+	public Member selectMemberByUserId(String userId) {
+		return memberRepository.selectMemberByUserId(userId);
+	}
 
 }
